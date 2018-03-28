d05a3fa09d497e6e9537fb7dd02969361a4fab0b
BackendMaster <fireballbg@abv.bg>
2018-03-28 05:21:25 +0300

    Initial changes

---
+++ src/config.js
@@ -0,0 +1,24 @@
+/*
+config.js - Configuration for ZENCash Coin
+*/
+
+
+module.exports = {
+    mainnet: {
+        messagePrefix: 'ZENCash main net',
+        bip32: {
+            public: '0488b21e',
+            private: '0488ade4'
+        },
+        pubKeyHash: '1cb8',
+        scriptHash: '1cbd',
+        zcPaymentAddressHash: '169a', // Private z-address
+        zcSpendingKeyHash: 'ab36', // Spending key
+        wif: '80'
+    },
+    testnet: {
+        wif: 'ef',
+        pubKeyHash: '1cb8',
+        scriptHash: '1cbd'
+    }
+};
